23756e240f29ff89c3b2ee35c06d97b031d932f2
doc <swapniltoppo@gmail.com>
2024-05-27 13:03:22 +0530

    login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
+import RegisterPage from './pages/Register'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -10,6 +15,7 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage/>}/>
       </Route>
     </Routes>
   )
