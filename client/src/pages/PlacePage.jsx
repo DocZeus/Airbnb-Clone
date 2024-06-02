76781a8c4c8852ffcd459abff1e455661524e0ee
doc <swapniltoppo@gmail.com>
2024-06-02 11:03:10 +0530

    Individual place page added

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import BookingWidget from "../BookingWidget";
 
 export default function PlacePage() {
     const { id } = useParams();
     const [place, setPlace] = useState(null);
+    const [showAllPhotos, setShowAllPhotos] = useState(false);
     useEffect(() => {
         if (!id) {
             return;
@@ -16,39 +18,87 @@ export default function PlacePage() {
 
     if (!place) return '';
 
+    if (showAllPhotos) {
+        return (
+            <div className="absolute inset-0 bg-white min-h-screen">
+                <div className="p-8 grid gap-4">
+                    <div>
+                        <button onClick={() => setShowAllPhotos(false)} className="fixed top-1 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>
+                            Close
+                        </button>
+                    </div>
+                    {place?.photos?.length > 0 && place.photos.map(photo => (
+                        <div key={photo}>
+                            <img src={'http://localhost:4000/uploads/' + photo} alt="" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className="mt-4 bg-gray-100 -mx-8 py-8 px-8">
+        <div className="mt-4 bg-gray-100 -mx-8 pt-8 px-8">
             <h1 className="text-3xl">{place.title}</h1>
-            <a className="my-2 block font-semibold underline"
+            <a className="flex gap-1 my-3 block font-semibold underline"
                 target="_blank"
-                href={"https://maps.google.com/?q=" + place.address}>{place.address}
+                href={"https://maps.google.com/?q=" + place.address}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                </svg>
+                {place.address}
             </a>
             <div className="relative">
-                <div className="grid gap-2 grid-cols-[2fr_1fr]">
+                <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
                     <div>
                         {place.photos?.[0] && (
                             <div>
-                                <img className="aspect-square object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} alt="" />
+                                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} alt="" />
                             </div>
                         )}
                     </div>
                     <div className="grid">
                         {place.photos?.[1] && (
-                            <img className="aspect-square object-cover" src={'http://localhost:4000/uploads/' + place.photos[1]} alt="" />
+                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[1]} alt="" />
                         )}
                         <div className="overflow-hidden">
                             {place.photos?.[2] && (
-                                <img className="aspect-square object-cover relative top-2" src={'http://localhost:4000/uploads/' + place.photos[2]} alt="" />
+                                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={'http://localhost:4000/uploads/' + place.photos[2]} alt="" />
                             )}
                         </div>
                     </div>
                 </div>
-                <button className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
+                <button onClick={() => setShowAllPhotos(true)}
+                    className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                     Show more photos
                 </button>
+            </div>
+            <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+                <div>
+                    <div className="my-4">
+                        <h2 className="my-2 font-semibold text-2xl">About this place</h2>
+                        {place.description}
+                    </div>
+                    Check-in: {place.checkIn}<br />
+                    Check-out: {place.checkOut}<br />
+                    Max number of guests: {place.maxGuests}
+                </div>
+                <div>
+                    <BookingWidget place={place} />
+                </div>
+            </div>
+            <div className="bg-white -mx-8 px-8 py-8 border-t">
+                <div>
+                    <h2 className="my-1 font-semibold text-2xl">Extra Info</h2>
+                </div>
+                <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
             </div>
         </div>
     )
